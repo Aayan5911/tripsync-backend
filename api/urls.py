c8a302ccbb7656232df28bdd1b03bb6561169810
Aayan5911 <aayan20070806@gmail.com>
2026-09-06 14:07:12 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,8 @@ from .views import (
     register_user, RegisterView,
     TripListCreateView, MemberPreferenceCreateView,
     ItineraryGenerateView, ExpenseListCreateView, ReviewCreateView,
-    send_email_otp, verify_email_otp
+    send_email_otp, verify_email_otp,
+    forgot_password_request, reset_password_confirm
 )
 
 urlpatterns = [
@@ -11,6 +12,8 @@ urlpatterns = [
     path('auth/login/', RegisterView.as_view(), name='login_user'),
     path('auth/send-email-otp/', send_email_otp, name='send_email_otp'),
     path('auth/verify-email-otp/', verify_email_otp, name='verify_email_otp'),
+    path('auth/forgot-password-request/', forgot_password_request, name='forgot_password_request'),
+    path('auth/reset-password-confirm/', reset_password_confirm, name='reset_password_confirm'),
     path('trips/', TripListCreateView.as_view(), name='trip-list-create'),
     path('preferences/', MemberPreferenceCreateView.as_view(), name='preference-create'),
     path('itinerary/<int:trip_id>/', ItineraryGenerateView.as_view(), name='itinerary-generate'),
